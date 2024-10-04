fa01e46b805cd438dd34261262473d93040a8d1c
Guilhermy Andrade <105890132+guilhermyandrade@users.noreply.github.com>
2024-10-03 22:02:57 -0300

    Configurando o Backend - Aula 03/10

--- mater-play-frontend/src/main.tsx
+++ mater-play-frontend/src/main.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { StrictMode } from 'react';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fff"
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
